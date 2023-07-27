c5f295a3e8e14ca8126ca632c71bc77d73f9daee
haytham918 <yunxuant@umich.edu>
2023-07-27 16:30:09 -0400

    Work on home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.css';
 
 const Header = () => {
+  const [Toggle, showMenu] = useState(false);
+
   return(
    <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">Haytham Tang</a>
 
-        <div className="nav__menu">
+        <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
           <ul className="nav__list grid">
 
             <li className="nav__item">
@@ -43,12 +45,12 @@ const Header = () => {
 
           </ul>
 
-          <i class="uil uil-times nav__close"></i>
+          <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
           
         </div>
 
-        <div className="nav__toggle">
-            <i class='uil uil-apps'></i>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+            <i className='uil uil-apps'></i>
           </div>
       </nav>
    </header>
---
+++ src/components/Social.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Social = () => {
+  return(
+    <div className="home__social">
+      <a href="https://linkedin.com" className="home__social__icon" target="_bliank">
+        <i className='uil uil-linkedin-alt'></i>
+      </a>
+      <a href="https://github.com/haytham918" className="home__social__icon" target="_bliank">
+        <i className='uil uil-github-alt'></i>
+      </a>
+      
+    </div>
+  )
+}
+
+export default Social;
