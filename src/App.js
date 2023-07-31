2dfe6cb4b10bdd4f1d68c1a30bcbb0671ec4d2c1
haytham918 <yunxuant@umich.edu>
2023-07-31 17:20:08 -0400

    Import footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 function App() {
   return (
    <>
@@ -10,6 +11,7 @@ function App() {
    <main className='main'>
     <Home />
     <Contact />
+    <Footer />
    </main>
    </>
   );
