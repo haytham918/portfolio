bc9a7a4c330a1b5b28cb777ba50a1bb701b78b63
haytham918 <yunxuant@umich.edu>
2023-08-05 02:03:47 -0400

    Fix some warnings

--- src/components/Capstone.jsx
+++ src/components/Capstone.jsx
@@ -4,10 +4,10 @@ import capstonePdf from '../assets/Linear-Programming-Capstone.pdf'
 export const Capstone = ({closeComponent}) => {
   return (
     <div className='detail-container'>
-      <a className='close-icon' onClick={closeComponent}>
+      <a href={() => false}className='close-icon' onClick={closeComponent}>
         <i className='uil uil-times close'></i>
       </a>
-      <iframe src='https://www.youtube.com/embed/j4yqDeWVyC8' width='85%' height='35%' className='youtube-video'>
+      <iframe title='Video of Capstone Presentation' src='https://www.youtube.com/embed/j4yqDeWVyC8' width='85%' height='35%' className='youtube-video'>
       </iframe>
       <h3 className='subtitle'>Video of Capstone Presentation</h3>
       <div className='text-part'>
