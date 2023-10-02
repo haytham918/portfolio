69a949d1383b121878e15bd66017b10b47475dd2
haytham918 <yunxuant@umich.edu>
2023-10-02 16:11:55 -0400

    Remove unused declaration

--- src/Converter/NavBar.jsx
+++ src/Converter/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./NavBar.css";
-import { useNavigate } from "react-router-dom";
 const NavBar = ({ gotoUnit, gotoCurrency, gotoTime }) => {
   return (
     <nav className="converter">
